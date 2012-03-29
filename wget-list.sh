3775edd7a8af648a605386a454ccbeb0a172fed1
djura-san <office@lab21.net>
2012-03-29 03:05:25 +0200

    wget-list.sh: fixed dl limit parameter (it is working now)

--- wget-list.sh
+++ wget-list.sh
@@ -6,49 +6,47 @@ dl_list="$HOME/tmp/download.txt"
 destination_dir="$HOME/downloads/general"
 #limit_rate="100k"
 
-main () {
-if [ ! -e "$dl_list" ]
- then
-  echo "ERROR: no $dl_list found!
+
+download_files () {
+if [ ! -e "$dl_list" ]; then
+   echo "ERROR: no $dl_list found!
 Please, read help for this script with \"$0 -h|--help\""
-  exit 2
+   exit 2
  fi
 
-if [ $# -gt 1 ]
- then
-   limit_rate="$(echo $2)"
-  else
-   limit_rate="0"
- fi
-
-while [ "$(find "$dl_list" -size +0)" ]
- do
-  dl_url="$(head -n1 "$dl_list")"
-  wget --limit-rate=$limit_rate -P "$destination_dir" -c "$dl_url" && sed -si 1d "$dl_list"  
+while [ "$(find "$dl_list" -size +0)" ]; do
+   dl_url="$(head -n1 "$dl_list")"
+   wget --limit-rate=$(echo $limit_rate)k -P "$destination_dir" -c "$dl_url" && sed -si 1d "$dl_list"  
  done
 }
 
 
-help () {
-echo "$0 - download files with wget by reading list with links
-File with download links is in $dl_list
 
-Usage: $0 limit_rate
+#set limit rate
+if [ ! -z "$1" ]; then
+    limit_rate="$1"
+ else
+    limit_rate="0"
+ fi
+
+#only one parameter!
+if [ "$#" -gt "1" ]; then
+   echo "Usage: $0 limit_rate
 
 limit_rate is your download limit for your connection defined in kbps
 Format is \"XXXXk\". Examples:
-   $0 100k
-   $0 256k
-   $0 1024k
-If no value, it will be set to 0 (max speed)
-"
-}
+  $0 100
+  $0 256
+  $0 1024
+
+If no value, it will be set to 0 (max speed)"
+   exit 1
+ fi
 
 
-case $@ in
- -d|--download ) main ;;
- -h|--help     ) help ;;
-  *            ) help ;;
-esac
+while true
+  do
+    download_files
+  done
 
 exit 0
