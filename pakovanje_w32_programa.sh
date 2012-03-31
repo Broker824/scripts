2f82888e06452055a305d81d2433b7d533ea690c
djura-san <office@lab21.net>
2012-03-31 14:41:09 +0200

    pakovanje_w32_programa.sh - dodavanje komande za uklanjanje razmaka iz imena datoteka i uklanjanje "loših" karaktera iz imena iste + sitne promene u kodu

--- pakovanje_w32_programa.sh
+++ pakovanje_w32_programa.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
+initial_directory="win32_packages_for_burning"
+packages_destination="zapakovani_programi"
+working_directory="kalab453"
 
 #pravljenje neophodnog direktorijuma u kojem će se sve zapakovati
-mkdir ./exe32
-mv ./*.exe ./exe32/
-mv ./*.msi ./exe32/
+mkdir $initial_directory
+#premeštanje svih exe i msi paketa
+mv *.exe $working_directory
+mv *.msi $working_directory
+#prebacivanje u radni direktorijum
+cd $working_directory
 
-cd ./exe32
-
-#petlja za pravljenje tar arhive (nekompresovane)
-for FAJL in *
+#petlja za pravljenje, nekompresovane, tar arhive
+for datoteka in *
   do
-    tar cf $FAJL.tar $FAJL
+    #uklanjanje "loših" karaktera iz imena datoteka
+    mv "$f" "`echo "$f" | tr -s " " "_" | tr "A-Z" "a-z"`" 2>/dev/null &
+    tar cf $datoteka.tar $FAJL
   done
 
 #prebacivanje svih tar arhiva u zaseban direktorijum
-mkdir ./tar
-mv ./*.tar ./tar/
+mkdir $packages_destination
+mv *.tar $packages_destination
