2403c290c30f7178e1465e2046db48ec65d04dff
djura-san <office@lab21.net>
2012-04-02 22:21:39 +0200

    pardel.sh: added colors and removed error messages from find query

--- pardel.sh
+++ pardel.sh
@@ -5,9 +5,11 @@ if [ $# -gt 1 ]; then
   query="$(echo $2)"
 
   #echo search results
-  echo "Files found: " && find "$directory" -iname "$query"
+  echo -e "Files found: \n"
+  #actually search
+  find "$directory" -iname "$query" 2>/dev/null | grep --color=auto "$query"
   #sanity check
-  echo "Are you sure that you want to delete these files? (Y/n)"; read answer
+  echo "Are you sure that you want to delete these files? (y/N)"; read answer
   if [ "$answer" = "[Y|y]" ]; then
     rm -f $(find $DIR -iname "$FILE")
   fi
@@ -21,4 +23,5 @@ Usage: $0 [directory] [file search query]
 Examples:
   $0 . bad_file.txt
   $0 /home/foo/temp_dir IMG_0457.JPEG" >&2
+
 fi
