89684252b0406091024fef92a7ee851f42106889
djura-san <office@lab21.net>
2012-04-09 13:25:56 +0200

    monsterwm_git_build.sh - minor tweak

--- monsterwm_git_build.sh
+++ monsterwm_git_build.sh
@@ -3,7 +3,7 @@ monsterwm_git_url="https://github.com/c00kiemon5ter/monsterwm"
 monsterwm_git_dir="/home/djura-san/programs/src_revision/git/"
 monsterwm_save_dir="/home/djura-san/programs/linux/wm/monsterwm"
 monsterwm_branch="monocleborders"
-personal_config="/home/djura-san/tmp/monsterwm/config.h"
+personal_config="/home/djura-san/projects/code/dotfiles-and-configs/monsterwm/config.h"
 
 #start
 if [ ! -d "$monsterwm_git_dir" ]; then
@@ -23,11 +23,10 @@ git clone -b $monsterwm_branch "$monsterwm_git_url" && cd monsterwm/
 if [ -e "$personal_config" ]; then cp -f "$personal_config" . && echo 'personal config copied to monsterwm'; fi
 
 #compile it
-echo && echo 'Enter root pass for installing...'
-su -c 'make clean install' || exit 1
+echo && echo -n 'Root '; su -c 'make clean install' || exit 1
 
 #save it for later but exclude .git files (and other dotfiles)
 echo "packaging...."
 if [ -e "$monsterwm_save_dir" ]; then
-    cd .. && tar cjf "$monsterwm_save_dir/monsterwm-$monsterwm_branch-git$(date +%d%m%Y_%H%M).tar.bz2" monsterwm/ --exclude '.*' || echo "there was an error with packaging latest monsterwm into tar.bz2 archive"
+    cd .. && tar cjf "$monsterwm_save_dir/monsterwm-$monsterwm_branch-git$(date +%d%m%Y_%H%M).tar.bz2" monsterwm/ --exclude '.*' || echo "there was an error with packaging latest monsterwm ($monsterwm_branch branch) into tar.bz2 archive"
   fi
