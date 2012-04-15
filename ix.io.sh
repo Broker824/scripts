63d1b6ca14ca6e04ac5615a67288e058f5804e58
djura-san <office@lab21.net>
2012-04-15 21:55:40 +0200

    sprunge.sh/ix.io.sh scripts updated

--- ix.io.sh
+++ ix.io.sh
@@ -2,9 +2,11 @@
 # api stuff is from http://ix.io/
 curl_parameters='-s'
 
-if [ -z "$2" ]; then paste_type="/$1";else paste_type="/$2"; fi
+if [ -z "$2" ]; then paste_type="$1";else paste_type="$2"; fi
+if [ -z "$paste_type" ]; then unset separator; else separator="/"; fi
+
 [[ -t 0 ]]  && url="$(curl $curl_parameters -n -F 'f:1=<-' http://ix.io <"$1")" \
             || url="$(curl $curl_parameters -n -F 'f:1=<-' http://ix.io)"
 
-printf "txt:%s %s\n" "$url$paste_type" "$*" >> /tmp/saved.urls
-xclip -i <<< "$url$paste_type" && xclip -o
+printf "txt:%s %s\n" "$url$separator$paste_type" "$*" >> /tmp/saved.urls
+xclip -i <<< "$url$separator$paste_type" && xclip -o
--- sprunge.sh
+++ sprunge.sh
@@ -2,10 +2,12 @@
 # api from http://sprunge.us/
 curl_parameters='-s'
 
-if [ -z "$2" ]; then paste_type="/$1";else paste_type="/$2"; fi
+if [ -z "$2" ]; then paste_type="$1";else paste_type="$2"; fi
+if [ -z "$paste_type" ]; then unset separator; else separator="?"; fi
+
 [[ -t 0 ]]  && url="$(curl $curl_parameters -sF 'sprunge=<-' http://sprunge.us <"$1")" \
             || url="$(curl $curl_parameters -sF 'sprunge=<-' http://sprunge.us)"
 
-printf "txt:%s %s\n" "$url$paste_type" "$*" >> /tmp/saved.urls
-xclip -i <<< "$url$paste_type" && xclip -o
+printf "txt:%s %s\n" "$url$separator$paste_type" "$*" >> /tmp/saved.urls
+xclip -i <<< "$url$separator$paste_type" && xclip -o
 
