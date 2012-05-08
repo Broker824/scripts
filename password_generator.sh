4cd0a3abf4d9ecb39dcedef706cebd805be7bc39
djura-san <office@lab21.net>
2012-05-08 02:34:57 +0200

    password_generator.sh - added option to generate random password length

--- password_generator.sh
+++ password_generator.sh
@@ -5,11 +5,13 @@ if [ $# -gt 0 ]
 then
   length="$(echo $1)"
 else
-  length="15"
+  #length="15"
+  #generate random number in range from 8 to 35
+  length="$[ ( $RANDOM % 35 ) + 8 ]"
 fi
 
 #define password complexity
-if [ -z "$2" ]; then
+if [ ! -z "$2" ]; then
   characters="a-zA-Z0-9"
 else
   characters="a-zA-Z0-9-_+?%()/#&$-"
